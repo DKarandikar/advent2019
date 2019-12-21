73c12a65a5dd48f9e9a33e1d292bb009f1a1d9d3
DKarandikar <daniel.karandikar@zoho.com>
2019-12-21 00:20:22 +0000

    Solve day 20 part 2 using 3d version of graph
    Switch to networkx for graphs rather than homegrown

--- src/day20.py
+++ src/day20.py
@@ -2,13 +2,13 @@ from collections import defaultdict
 from typing import Tuple
 
 import numpy as np
+import networkx as nx
 
 from src.inputHelpers.text_grids import parse_text_grid_to_numpy_array
-from src.logicHelpers.graph import Graph
 
 
-def createGraphPart1(data: np.array) -> Tuple[Graph, Tuple[int, int], Tuple[int, int]]:
-    g = Graph()
+def createGraphPart1(data: np.array) -> Tuple[nx.Graph, Tuple[int, int], Tuple[int, int]]:
+    g = nx.Graph()
     teleports = defaultdict(list)
     edges, extTeleports, intTeleports = get_edges_and_portals(data)
 
@@ -18,12 +18,12 @@ def createGraphPart1(data: np.array) -> Tuple[Graph, Tuple[int, int], Tuple[int,
         teleports[teleportCode].append(location)
 
     for edgePair in edges:
-        g.addEdge(edgePair[0], edgePair[1])
+        g.add_edge(edgePair[0], edgePair[1])
 
     for key, teleport in teleports.items():
         if len(teleport) == 2:
-            g.addEdge(teleport[0], teleport[1])
-            g.addEdge(teleport[1], teleport[0])
+            g.add_edge(teleport[0], teleport[1])
+            g.add_edge(teleport[1], teleport[0])
         elif len(teleport) == 1:
             # start or end
             pass
@@ -31,6 +31,24 @@ def createGraphPart1(data: np.array) -> Tuple[Graph, Tuple[int, int], Tuple[int,
             raise RuntimeError(f"Too many points for teleport: {key}")
 
     return g, teleports.get(("A", "A"))[0], teleports.get(("Z", "Z"))[0]
+
+
+def createGraphPart2(data: np.array) -> Tuple[nx.Graph, Tuple[int, int], Tuple[int, int]]:
+    g = nx.Graph()
+    edges, extTeleports, intTeleports = get_edges_and_portals(data)
+
+    for layer in range(500):
+        for edgePair in edges:
+            g.add_edge((*edgePair[0], layer), (*edgePair[1], layer))
+
+        for name, location in intTeleports.items():
+            g.add_edge((*location, layer), (*extTeleports.get(name), layer + 1))
+
+        for name, location in extTeleports.items():
+            if layer != 0 and name != ("A", "A") and name != ("Z", "Z"):
+                g.add_edge((*location, layer), (*intTeleports.get(name), layer - 1))
+
+    return g, (*extTeleports.get(("A", "A")), 0), (*extTeleports.get(("Z", "Z")), 0)
 
 
 def get_edges_and_portals(data):
@@ -63,12 +81,22 @@ def get_edges_and_portals(data):
 def get_shortest_distance_start_to_end(filename: str):
     data = parse_text_grid_to_numpy_array(filename)
     g, start, end = createGraphPart1(data)
-    return len(g.BFS(start, end).history)
+    rv = nx.shortest_path(g, source=start, target=end)
+    return len(rv) - 1
+
+
+def get_shortest_distance_start_to_end_layers(filename: str):
+    data = parse_text_grid_to_numpy_array(filename)
+    g, start, end = createGraphPart2(data)
+    rv = nx.shortest_path(g, source=start, target=end)
+    return len(rv) - 1
 
 
 def main():
     part1 = get_shortest_distance_start_to_end(".././inputData/day20part1")
     print(f"Part 1 shortest is {part1}")
+    part2 = get_shortest_distance_start_to_end_layers(".././inputData/day20part1")
+    print(f"Part 2 shortest is {part2}")
 
 
 if __name__ == "__main__":
--- tests/day20Tests.py
+++ tests/day20Tests.py
@@ -1,4 +1,4 @@
-from src.day20 import get_shortest_distance_start_to_end
+from src.day20 import get_shortest_distance_start_to_end, get_shortest_distance_start_to_end_layers
 
 
 def test_day_20_small():
@@ -9,3 +9,13 @@ def test_day_20_small():
 def test_day_20_medium():
     rv = get_shortest_distance_start_to_end("../inputData/day20test2")
     assert rv == 58
+
+
+def test_day_20_small_layers():
+    rv = get_shortest_distance_start_to_end_layers("../inputData/day20test1")
+    assert rv == 26
+
+
+def test_day_20_medium_layers():
+    rv = get_shortest_distance_start_to_end_layers("../inputData/day20test3")
+    assert rv == 396
